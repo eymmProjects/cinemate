0fe671671c7924d1ba856ffd50d92e851d93cffb
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-05-27 00:00:03 +0800

    Deployment

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { Button } from "./Button";
+
+export { Header } from "./Header";
+export { Footer } from "./Footer";
+
+export { Card } from "./Card";
+export { ScrollToTop } from "./ScrollToTop";
